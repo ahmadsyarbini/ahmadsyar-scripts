0bbbce9109f2d113518fc8fbf4510bd4e587b7d5
Ahmad Syarbini <ahmadsyar@gmail.com>
2026-04-22 16:29:56 +0800

    update

--- others/script-adobe-backup-to-mega.cmd
+++ others/script-adobe-backup-to-mega.cmd
@@ -2,7 +2,8 @@
 Title adobe to mega backupper
 rem usage copy paste into adobe folder
 rclone cleanup megarepo:
-rclone sync "%~dp0." megarepo:/Lib-Adobe --include "Adobe*.iso" --ignore-case -P --bwlimit 4M
-rclone sync "%~dp0." megarepo:/Lib-Adobe --include "*.md5" --ignore-case -P --bwlimit 4M
+rclone sync "%~dp0." megarepo:/Lib-Adobe --include --ignore-case -P --bwlimit 4M
+rem rclone sync "%~dp0." megarepo:/Lib-Adobe --include "Adobe*.iso" --ignore-case -P --bwlimit 4M
+rem rclone sync "%~dp0." megarepo:/Lib-Adobe --include "*.md5" --ignore-case -P --bwlimit 4M
 rclone cleanup megarepo:
 timeout /t 2 >nul
