a565c1830bf9246b0380336e7de138e8a0b1a1da
Ahmad Syarbini <ahmadsyar@gmail.com>
2026-04-27 14:32:51 +0800

    update

--- others/sc-installer-ejie.me.clover.cmd
+++ others/sc-installer-ejie.me.clover.cmd
@@ -2,7 +2,10 @@
 curl -L -o"%temp%\clover-setup.exe" "https://github.com/ahmadsyarbini/clover-backup/raw/refs/heads/main/setup_clover_3.5.4.exe"
 cd %temp%
 clover-setup.exe /S
+timeout /t 1 /nobreak >nul
+taskkill /IM "clover-setup.exe" /T /F
+timeout /t 1 /nobreak >nul
 cd %temp%
 if exist "%temp%\clover-setup.exe" del /q "%temp%\clover-setup.exe"
-timeout /t 5 /nobreak >nul
+timeout /t 3 /nobreak >nul
 taskkill /IM "clover-setup.exe" /T /F
