99aba031f3e9a0ec879dfbe049001816e930beef
Ahmad Syarbini <ahmadsyar@gmail.com>
2026-05-14 06:21:37 +0800

    update

--- set-once/script-create-tasksch-local-repo-updater.cmd
+++ set-once/script-create-tasksch-local-repo-updater.cmd
@@ -10,21 +10,38 @@ set "xmlFile=%xmlDir%\amr-scripts-update.xml"
 md "%xmlDir%" 2>nul
 
 > "%xmlFile%" (
-  echo ^<Task version="1.4" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task"^>
+  echo ^<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task"^>
   echo   ^<Triggers^>
   echo     ^<CalendarTrigger^>
   echo       ^<StartBoundary^>2026-05-15T06:00:00^</StartBoundary^>
+  echo       ^<Enabled^>true^</Enabled^>
   echo       ^<ScheduleByDay^>^<DaysInterval^>1^</DaysInterval^>^</ScheduleByDay^>
   echo     ^</CalendarTrigger^>
   echo   ^</Triggers^>
   echo   ^<Principals^>
   echo     ^<Principal id="Author"^>
   echo       ^<LogonType^>InteractiveToken^</LogonType^>
+  echo       ^<RunLevel^>LeastPrivilege^</RunLevel^>
   echo     ^</Principal^>
   echo   ^</Principals^>
   echo   ^<Settings^>
   echo     ^<MultipleInstancesPolicy^>IgnoreNew^</MultipleInstancesPolicy^>
+  echo     ^<DisallowStartIfOnBatteries^>false^</DisallowStartIfOnBatteries^>
+  echo     ^<StopIfGoingOnBatteries^>false^</StopIfGoingOnBatteries^>
+  echo     ^<AllowHardTerminate^>true^</AllowHardTerminate^>
   echo     ^<StartWhenAvailable^>true^</StartWhenAvailable^>
+  echo     ^<RunOnlyIfNetworkAvailable^>false^</RunOnlyIfNetworkAvailable^>
+  echo     ^<IdleSettings^>
+  echo       ^<StopOnIdleEnd^>true^</StopOnIdleEnd^>
+  echo       ^<RestartOnIdle^>false^</RestartOnIdle^>
+  echo     ^</IdleSettings^>
+  echo     ^<AllowStartOnDemand^>true^</AllowStartOnDemand^>
+  echo     ^<Enabled^>true^</Enabled^>
+  echo     ^<Hidden^>false^</Hidden^>
+  echo     ^<RunOnlyIfIdle^>false^</RunOnlyIfIdle^>
+  echo     ^<WakeToRun^>false^</WakeToRun^>
+  echo     ^<ExecutionTimeLimit^>PT3H^</ExecutionTimeLimit^>
+  echo     ^<Priority^>7^</Priority^>
   echo   ^</Settings^>
   echo   ^<Actions Context="Author"^>
   echo     ^<Exec^>
