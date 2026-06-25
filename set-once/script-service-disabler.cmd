a1795b2bf46228b74d75a30c2ab370fa6534cb91
Ahmad Syarbini <ahmadsyar@gmail.com>
2026-06-25 23:51:31 +0800

    update

---
+++ set-once/script-service-disabler.cmd
@@ -0,0 +1,10 @@
+@echo off
+rem -------read-------
+rem some service disabler. not sure if needed or not.
+rem -------read-------
+rem Admin priv elevator
+net session >nul 2>&1 || (powershell -c "Start-Process '%~f0' -Verb RunAs" & exit /b)
+rem End of admin elevator
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\CDPSvc" /v Start /t REG_DWORD /d 4 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\CDPUserSvc" /v Start /t REG_DWORD /d 4 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\CDPUserSvc" /v UserServiceFlags /t REG_DWORD /d 0 /f
