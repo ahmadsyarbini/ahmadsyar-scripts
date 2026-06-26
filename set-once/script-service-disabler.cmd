6aa28ac4d206ee6e7359d7eee813c27adda5db9a
Ahmad Syarbini <ahmadsyar@gmail.com>
2026-06-26 12:07:29 +0800

    update

--- set-once/script-service-disabler.cmd
+++ set-once/script-service-disabler.cmd
@@ -5,7 +5,11 @@ rem -------read-------
 rem Admin priv elevator
 net session >nul 2>&1 || (powershell -c "Start-Process '%~f0' -Verb RunAs" & exit /b)
 rem End of admin elevator
+
+rem disable connected device and platform
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\CDPSvc" /v Start /t REG_DWORD /d 4 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\DiagTrack" /v Start /t REG_DWORD /d 4 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\CDPUserSvc" /v Start /t REG_DWORD /d 4 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\CDPUserSvc" /v UserServiceFlags /t REG_DWORD /d 0 /f
 echo done
