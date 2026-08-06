9d6e633318194e94741f517902222dc6df8f6bb0
ahmadsyarbini <ahmadsyar@gmail.com>
2026-08-06 08:54:53 +0800

    update

--- switch-nvidia-LS.cmd
+++ switch-nvidia-LS.cmd
@@ -15,21 +15,15 @@ if %errorlevel% == 2 goto turnoff
 if %errorlevel% == 1 goto turnon
 
 :turnoff
-taskkill /IM "NVIDIA App.exe" /T /F
-taskkill /IM "nvcontainer.exe" /T /F
+taskkill /IM "nvcplui.exe" /T /F
 net stop "NVDisplay.ContainerLocalSystem"
-net stop "NvContainerLocalSystem"
 sc config "NVDisplay.ContainerLocalSystem" start= disabled
-sc config "NvContainerLocalSystem" start= disabled
 goto end
 
 :turnon
 sc config "NVDisplay.ContainerLocalSystem" start= demand
-sc config "NvContainerLocalSystem" start= demand
 net start "NVDisplay.ContainerLocalSystem"
-net start "NvContainerLocalSystem"
-rem start "" "C:\Program Files\WindowsApps\NVIDIACorp.NVIDIAControlPanel_8.1.969.0_x64__56jybvy8sckqj\nvcplui.exe"
-start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\NVIDIA Corporation\NVIDIA App.lnk"
+start "" "C:\Program Files\WindowsApps\NVIDIACorp.NVIDIAControlPanel_8.1.969.0_x64__56jybvy8sckqj\nvcplui.exe"
 goto end
 
 :end
