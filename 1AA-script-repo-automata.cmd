f05fafc3316522c60dc9e32d311ad61a938df6ec
ahmadsyarbini <ahmadsyar@gmail.com>
2026-09-07 13:48:58 +0800

    update

--- 1AA-script-repo-automata.cmd
+++ 1AA-script-repo-automata.cmd
@@ -7,20 +7,24 @@ rem targets
 rem set "targetpdrive=F:\Tech"
 set "localfolder=%userprofile%\amr-scripts\Tech"
 
-rem pendrive drive letter checker
+rem ------------------- pendrive drive letter checker ---------------------
 
 for /f "delims=" %%D in ('powershell -NoProfile -Command "Get-Volume | Where-Object { $_.DriveLetter -and (Test-Path ($_.DriveLetter + ':\amr-drive.txt')) } | Select-Object -First 1 -ExpandProperty DriveLetter"') do set "DRIVE=%%D:"
+
+rem for /f "delims=" %%D in ( ^
+rem     'powershell -NoProfile -Command "Get-Volume ^| Where-Object { $_.DriveLetter -and (Test-Path ($_.DriveLetter + ':\amr-drive.txt')) } ^| Select-Object -First 1 -ExpandProperty DriveLetter"' ^
+rem ) do set "DRIVE=%%D:"
 
 if not defined DRIVE (
     echo Target pendrive not found. proceed with git sync
     echo.
-    timeout /t 2 /nobreak >nul
+    timeout /t 1 /nobreak >nul
 )
 
 echo Target pendrive: %DRIVE%
 echo.
 
-rem mirror to targets
+rem ------------------- mirror to pendrive targets -------------------
 
 rem local source condition, mirror from source to pendrive
 if exist "%SourceWinScript%" (
@@ -29,15 +33,6 @@ if exist "%SourceWinScript%" (
 ) else (
     echo missing: "%SourceWinScript%" or "%DRIVE%\Tech"
 )
-
-rem simpan jap
-rem rem local source condition, mirror from source to pendrive
-rem if exist "%SourceWinScript%" (
-rem     if exist "%targetpdrive%" (
-rem         robocopy "%SourceWinScript%" "%targetpdrive%" "*" /MIR /DCOPY:D /COPY:D /W:1 /R:1 /NDL /NJH /NJS /XD ".git"
-rem ) else (
-rem     echo missing: "%SourceWinScript%" or "%targetpdrive%"
-rem )
 
 )
 rem condition remote pc with internet exist, mirror github repo to local
@@ -56,4 +51,4 @@ if exist "%SourceWinScript%" (
 )
 
 rem pause
-timeout /t 2 /nobreak >nul
+timeout /t 1 /nobreak >nul
