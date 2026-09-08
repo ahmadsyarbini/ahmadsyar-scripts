77d1ea2f5ad07b4731d8cc3edc430ada1f0aa12a
ahmadsyarbini <ahmadsyar@gmail.com>
2026-09-08 20:46:48 +0800

    update

--- others/vmware-tools-silent.cmd
+++ others/vmware-tools-silent.cmd
@@ -5,6 +5,11 @@ rem End of admin elevator
 
 rem README Vmware tool quick silent installer for windows 11 and 10.
 rem usage : Disconnect OS iso first, then mount the vmware tools iso using the "install vmware tools" menu
+rem ignore this trying new method
 
-"D:\setup.exe" /s /v"/qn ADDLOCAL=ALL REBOOT=Force"
+for /f "delims=" %%D in ('powershell -NoProfile -Command "Get-Volume ^| Where-Object { $_.DriveLetter -and (Test-Path ($_.DriveLetter + ':\setup.exe')) } ^| Select-Object -First 1 -ExpandProperty DriveLetter"') do set "VMTOOLS=%%D:\setup.exe"
+
+echo Found: "%VMTOOLS%"
+
+"%VMTOOLS%" /S /v"/qb ADDLOCAL=ALL REBOOT=Force"
 timeout /t 5 /nobreak >nul
