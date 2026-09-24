305d299cd2a112e779045b2919d3b8d48be06a60
ahmadsyarbini <ahmadsyar@gmail.com>
2026-09-24 20:40:52 +0800

    update

--- set-once/script-create-sharpkey-remap.cmd
+++ set-once/script-create-sharpkey-remap.cmd
@@ -4,6 +4,6 @@ net session >nul 2>&1 || (powershell -c "Start-Process '%~f0' -Verb RunAs" & exi
 :: End of admin elevator
 rem for mapping left alt to left ctrl
 rem for mapping scroll lock to f13
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d 0000000000000000030000001D0038006D00460000000000 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d 0000000000000000030000001D0038006400460000000000 /f
 echo Left Alt remapped to Left Ctrl. Reboot required.
 timeout /t 3 /nobreak >nul
